5b7632c2f70ed20ed52fc2694a9e0cd1f6fc6d61
pwin <p.w@inbox.com>
2014-08-12 00:16:18 +0100

    updating to most recent versions of dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject alida "0.1.3"
+(defproject alida "pw0.1.3"
   :description "Crawling, scraping and indexing application."
-  :url "https://github.com/fmw/alida"
+  :url "https://github.com/pwin/alida"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1011"]
-                 [org.clojure/data.json "0.1.2"]
-                 [clj-http "0.4.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [org.jsoup/jsoup "1.6.1"]
-                 [com.ashafa/clutch "0.4.0-SNAPSHOT"]
-                 [clj-time "0.3.7"]
-                 [org.apache.lucene/lucene-core "4.0.0-ALPHA"]
-                 [org.apache.lucene/lucene-queryparser "4.0.0-ALPHA"]
-                 [org.apache.lucene/lucene-analyzers-common "4.0.0-ALPHA"]
-                 [clj-http-fake "0.3.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/data.json "0.2.5"]
+                 [clj-http "1.0.0"]
+                 [enlive "1.1.5"]
+                 [org.jsoup/jsoup "1.7.3"]
+                 [com.ashafa/clutch "0.4.0-RC1"]
+                 [clj-time "0.8.0"]
+                 [org.apache.lucene/lucene-core "4.9.0"]
+                 [org.apache.lucene/lucene-queryparser "4.9.0"]
+                 [org.apache.lucene/lucene-analyzers-common "4.9.0"]
+                 [clj-http-fake "0.7.8"]]
   :exclusions [lein-swank swank-clojure])
